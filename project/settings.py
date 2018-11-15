c30e08272f08d3d9870ed7fc4e772af31731c929
tz466731927@126.com <tz44559658>
2018-11-15 15:11:28 +0800

    modify setting.py. add admin static dir.

--- project/settings.py
+++ project/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = 'n)lbjdr9-eb6x&^!5ymn%p4p1t5x@b6nh6gow(_nlw2x2)=x4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.tangzheng.info', '101.132.115.240']
 
@@ -132,4 +133,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# STATIC_URL = '/static/'
 STATIC_URL = 'http://101.132.115.240:8080/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
