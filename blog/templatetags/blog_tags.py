43a0e8f064cf8b2aff49353f4c7250992b0af310
tz466731927@126.com <tz44559658>
2018-11-15 19:18:26 +0800

    count category number.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -4,7 +4,9 @@
 # @Author  : Alvin
 # @File    : blog_tags.py
 from django import template  #导入template模块
-from ..models import Post, Category
+from ..models import Post, Category, Tag
+
+from django.db.models.aggregates import Count
 
 register = template.Library()   #实例化template.Library()类
 
@@ -19,4 +21,12 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    # return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+"""
+@register.simple_tag
+def get_tags()
+    # Count 计算分类下的文章数，其接受的参数为需要计数的模型的名称
+    return Tag.objects.annotate(num_posts=Count('post'))
+"""
